0e4dfd2ae51379ab8bfea839079306300acbdeef
ralphHuerta <huerta.1996@hotmail.com>
2022-04-27 22:10:36 -0500

    cambio de ruta buscar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,15 +4,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { CreateComponent } from './components/create/create.component';
-import { DetailComponent } from './components/detail/detail.component';
 import { TestComponent } from './components/test/test.component';
 import { VigilanteGuard } from './vigilante.guard';
 const routes: Routes = [
   {path: '', component: HomeComponent, canActivate:[VigilanteGuard]},
   {path: 'login', component: LoginComponent},
   {path: 'home', component:HomeComponent, canActivate:[VigilanteGuard]},
-  {path: 'buscar', component: CreateComponent, canActivate:[VigilanteGuard]},
-  {path: 'deatalils/:id', component: DetailComponent ,canActivate:[VigilanteGuard]},
+  {path: 'search', component: CreateComponent, canActivate:[VigilanteGuard]},
   {path: 'test', component: TestComponent}
 ];
 
